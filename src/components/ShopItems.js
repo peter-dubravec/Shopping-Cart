2bd6cb38107304e82920ae2f4d8313d6c019078b
Peter Dubravec <peter.dubravec98@gmail.com>
2022-12-25 14:44:39 +0100

    Finished Project

--- src/components/ShopItems.js
+++ src/components/ShopItems.js
@@ -10,12 +10,12 @@ import {
 
 const ShopItems = ({ addToCart }) => {
   const items = [
-    { name: "Sweater", price: 50, quantity: 1, img: image1, id: 1 },
-    { name: "Sweater", price: 35, quantity: 1, img: image2, id: 2 },
-    { name: "Sweater", price: 89, quantity: 1, img: image3, id: 3 },
-    { name: "Sweater", price: 47, quantity: 1, img: image4, id: 4 },
-    { name: "Sweater", price: 85, quantity: 1, img: image5, id: 5 },
-    { name: "Sweater", price: 76, quantity: 1, img: image6, id: 6 },
+    { name: "Shirt", price: 50, quantity: 1, img: image1, id: 1 },
+    { name: "Red Sweater", price: 35, quantity: 1, img: image2, id: 2 },
+    { name: "Green Sweater", price: 89, quantity: 1, img: image3, id: 3 },
+    { name: "Red Sweater", price: 47, quantity: 1, img: image4, id: 4 },
+    { name: "Pink Sweater", price: 85, quantity: 1, img: image5, id: 5 },
+    { name: "Black Sweater", price: 76, quantity: 1, img: image6, id: 6 },
   ];
 
   return (
